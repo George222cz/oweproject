f1847441f51595f33c27d74326c8d36b2e876024
George222cz <georgedrimal@seznam.cz>
2019-04-10 13:37:54 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
+import exampleJSON from './json1.json';
+console.log(exampleJSON);
 
 class App extends Component {
 
+/*
     state = {
         patient: {},
-    };
-
+    };*/
+/*
     componentDidMount() {
         fetch("URL here!!!!")
             .then((response) => response.json())
             .then((jsonResponse) => {
                 this.setState({patient: jsonResponse})
             }).catch((err) => console.error(err));
-    }
+    }*/
+
+  /*  componentDidMount() {
+        this.setState({patient: exampleJSON});
+    }*/
 
   render() {
-      const {patient} = this.state;
-      const symptoms = patient.symptoms || {};
-      const personalData = patient.personalData || {};
-      const medicaments = patient.pharmaceuticals || [];
+   /*   const patient = this.state;
+      console.log(patient);
+      const  diagnosis = patient.diagnosis || {};*/
+ /*     if(diagnosis==={}){
+          return (<div><p>fck!!!</p></div>);
+      }*/
     return (
       <div className="App">
         <header className="App-header">
-            <p>{symptoms}</p>
-            <p>{personalData}</p>
-            <p>{medicaments}</p>
+            <h1>{exampleJSON.diagnosis}</h1>
         </header>
       </div>
     );
   }
 }
+
 
 export default App;
