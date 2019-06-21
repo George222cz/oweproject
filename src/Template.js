80852a6d5e79a948cbd28f20b50cc4a3e4d5256c
George222cz <georgedrimal@seznam.cz>
2019-06-21 17:53:31 +0200

    fuuuuuuuck...pred predelanim!

--- src/Template.js
+++ src/Template.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Generator from "./components/Generator";
+import './Main.css';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 export default class Template extends React.Component {
-    state = {
-        size: 0,
-        type: 1
-    };
+    constructor () {
+        super();
+        this.state = {
+            size: 0,
+            type: 1,
+            selected: {value: '', label: 'Select'},
+            diagnoses: [],
+            data: [{title: ''}],
+            title: ''
+        };
+        this._onSelect = this._onSelect.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("https://owe-kazu.herokuapp.com/api/rest/student/diagnosis")
+            .then((response) => response.json())
+            .then((jsonResponse) => {
+                console.log(jsonResponse);
+                this.setState({diagnoses: jsonResponse})
+            }).catch((err) => alert(err));
+    }
 
     addGenerator = (type) => {
         this.setState({
@@ -14,38 +35,63 @@ export default class Template extends React.Component {
         });
     };
 
+    _onSelect (option) {
+        //console.log('You selected ', option.value);
+        this.setState({selected: option});
+    };
+
+    handleOnChange = (type) => (event) => {
+        console.log(event.target.value);
+        //this.setState({[type]: event.target.value});
+        this.setState({ [event.target.name]: event.target.value });
+        console.log(this.state.data[0].title);
+    };
+    // Ucitel muze vytvorit nove generatory...vybere si jeden ze 3 typu (MIN/MAX, TEXT, IMAGE) podle toho mu to hodi form
+    // Exam je zobrazeni pro studenta true-> buttonek v aplikaci pro zobrazení false-> zobrazení od začátku
+    //Malus - smrt, bonus - body úspěchu a price jsou krajní hodnoty pro úspěch/neúspěch..
+    // ucitel vytvori template, ten odesleme do template collection a z toho potom student dostane random pacienta
+    // template se sklada z generatoru!!
     render() {
         const generators = [];
-
-        for (var i = 0; i < this.state.size; i += 1) {
-            generators.push(<Generator type={this.state.type} />);
+        const {title} = this.state.title;
+        for (let i = 0; i < this.state.size; i += 1) {
+            generators.push(<Generator type={this.state.type} title={title} handleChange={this.handleOnChange}/>);
         }
-        // Ucitel muze vytvorit nove generatory...vybere si jeden ze 3 typu (MIN/MAX, TEXT, IMAGE) podle toho mu to hodi form
-        // Exam je zobrazeni pro studenta true-> buttonek v aplikaci pro zobrazení false-> zobrazení od začátku
-        //Malus - smrt, bonus - body úspěchu a price jsou krajní hodnoty pro úspěch/neúspěch..
-        // ucitel vytvori template, ten odesleme do template collection a z toho potom student dostane random pacienta
-        // template se sklada z generatoru!!
+        const options = this.state.diagnoses;
+        const defaultOption = this.state.selected;
+        const selected = this.state.selected.value;
+     //   const price = this.state.price;
         return (
-        <div>
+        <div className="inlineP">
             <div>
             <h2>Create new template</h2>
-            <p>diagnosis:</p>
+            <div style={{display: 'inline-flex'}}>
+                <p>diagnosis: </p>
+                <div style={{marginLeft: '4%', fontSize: '1em'}}>
+                <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+                </div>
+            </div>
+            <p style={{marginLeft: '20px'}}>maxMalus: </p>
             <input type={"text"}/>
-            <p>maxMalus:</p>
+            <p>maxBonus: </p>
             <input type={"text"}/>
-            <p>maxBonus:</p>
+            <p>maxPrice: </p>
             <input type={"text"}/>
-            <p>maxPrice:</p>
-            <input type={"text"}/>
+
             </div>
             <div>
-                <p>add new generator</p>
+                <p>add new generator: </p>
                 <button onClick={() => this.addGenerator(1)}>Add generator1</button>
                 <button onClick={() => this.addGenerator(2)}>Add generator2</button>
+                <button onClick={() => this.addGenerator(3)}>Add generator3</button>
+                <br></br>
+                <br></br>
                 <div>
                     {generators}
                 </div>
             </div>
+            <p>{selected}</p>
+            <button>Submit</button>
         </div>
         );
     }
--- src/components/Generator/Generator.js
+++ src/components/Generator/Generator.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Switch from "react-switch";
 
 export default class Template extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            type: props.type
-        }
+            type: props.type,
+            checked: false
+        };
     }
 
     pStyle = {
@@ -14,28 +16,50 @@ export default class Template extends React.Component {
 
     };
 
+    handleChange(checked) {
+        this.setState({ checked });
+    }
+
     render() {
-        const {type} = this.state;
+        const {type, checked} = this.state;
+        const {title,handleChange} = this.props;
         return (
             <div>
-            <div style={{backgroundColor: 'gray'}}>
-                <p style={this.pStyle}>title:</p>
-                <input type={"text"}/>
-                {(type === 1) &&
+                <div style={{backgroundColor: '#5a5a5a'}}>
+                    <p style={this.pStyle}>Title: </p>
+                    <input type={"text"} value={title} onChange={handleChange('data.title')}/>
+                    {(type === 1) &&
                     <div>
-                        <p style={this.pStyle}>text:</p>
+                        <p style={this.pStyle}>Text: </p>
                         <input type={"text"}/>
                     </div>
-                }
-                {(type === 2) &&
-                <div>
-                    <p style={this.pStyle}>Min:</p>
-                    <input type={"text"}/>
-                    <p style={this.pStyle}>Max:</p>
-                    <input type={"text"}/>
+                    }
+                    {(type === 2) &&
+                    <div>
+                        <p style={this.pStyle}>Min: </p>
+                        <input type={"text"}/>
+                        <p style={this.pStyle}>Max: </p>
+                        <input type={"text"}/>
+                    </div>
+                    }
+                    {(type === 3) &&
+                    <div>
+                        <p style={this.pStyle}>Images: </p>
+                    </div>
+                    }
+                    <p style={this.pStyle}>exam: </p>
+                    <Switch onChange={this.handleChange} checked={this.state.checked}/>
+                    {(checked) &&
+                    <div>
+                        <p style={this.pStyle}>Malus: </p>
+                        <input type={"text"}/>
+                        <p style={this.pStyle}>Bonus: </p>
+                        <input type={"text"}/>
+                        <p style={this.pStyle}>Price: </p>
+                        <input type={"text"}/>
+                    </div>
+                    }
                 </div>
-                }
-            </div>
                 <br></br>
             </div>
         );
